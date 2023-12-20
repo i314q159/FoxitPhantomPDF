fb1018d4ad24b526ff34dc068fc9712f654de61d
i314q159 <i314q159@outlook.com>
2023-12-20 16:21:47 +0800

    亿赛通解密

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::fs;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn read_bytes(filepath: &str) -> Result<Vec<u8>, std::io::Error> {
+    let mut file = fs::File::open(filepath)?;
+    let mut data = Vec::new();
+    file.read_to_end(&mut data)?;
+    Ok(data)
+}
+
+fn save_bytes(bytes: &[u8], target_path: &str) -> Result<(), std::io::Error> {
+    let mut file = fs::File::create(target_path)?;
+    file.write_all(bytes)?;
+    Ok(())
+}
+
+fn double_file_extension(source_path: &str) -> String {
+    let path = Path::new(source_path);
+    let extension = path.extension().unwrap_or_default();
+    let stem = path.file_stem().unwrap_or_default();
+    format!("{}.{}{}", stem.to_str().unwrap(), extension.to_str().unwrap(), extension.to_str().unwrap())
+}
+
+fn main() {
+    let source_path = "a.xlsx";
+    let target_path = double_file_extension(source_path);
+
+    if let Err(error) = read_bytes(source_path).and_then(|content| save_bytes(&content, &target_path)) {
+        println!("Error: {}", error);
+    } else {
+        println!("File successfully saved to: {}", target_path);
+    }
+}
