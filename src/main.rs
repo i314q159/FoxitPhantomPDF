73d1f43b5d64ec4751f9dbd5db7577c9297e3c2d
i314q159 <i314q159@outlook.com>
2023-12-21 08:39:18 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::fs;
 use std::io::prelude::*;
 use std::path::Path;
+use std::{env, fs};
 
 fn read_bytes(filepath: &str) -> Result<Vec<u8>, std::io::Error> {
     let mut file = fs::File::open(filepath)?;
@@ -19,14 +19,29 @@ fn double_file_extension(source_path: &str) -> String {
     let path = Path::new(source_path);
     let extension = path.extension().unwrap_or_default();
     let stem = path.file_stem().unwrap_or_default();
-    format!("{}.{}{}", stem.to_str().unwrap(), extension.to_str().unwrap(), extension.to_str().unwrap())
+    format!(
+        "{}.{}{}",
+        stem.to_str().unwrap(),
+        extension.to_str().unwrap(),
+        extension.to_str().unwrap()
+    )
 }
 
 fn main() {
-    let source_path = "a.xlsx";
+    let args: Vec<String> = env::args().collect();
+
+    let source_path = &args[1];
+
+    if args.len() != 2 {
+        println!("Usage: program_name file_name");
+        return;
+    }
+
     let target_path = double_file_extension(source_path);
 
-    if let Err(error) = read_bytes(source_path).and_then(|content| save_bytes(&content, &target_path)) {
+    if let Err(error) =
+        read_bytes(source_path).and_then(|content| save_bytes(&content, &target_path))
+    {
         println!("Error: {}", error);
     } else {
         println!("File successfully saved to: {}", target_path);
